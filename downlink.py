52835c81afc0100b7ce8d943f554e48c771c9ccf
SuperHelios <20555025+Imperator26@users.noreply.github.com>
2020-02-22 16:48:16 +0100

    Find events for a satellite

---
+++ downlink.py
@@ -0,0 +1,67 @@
+import logging
+import yaml
+import asyncio
+import subprocess as sp
+
+from datetime import datetime, timedelta, timezone
+from skyfield.api import Topos, Loader
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    '[%(asctime)s] - %(funcName)s: %(message)s',
+    '%H:%M:%S'
+)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+fh = logging.FileHandler('logs/downlink.log')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+
+with open('settings.yaml', 'r') as file:
+    try:
+        settings = yaml.safe_load(file)
+    except yaml.YAMLError as e:
+        logger.error(e)
+
+logger.info('Position set to:')
+logger.info(f'Latitude: {settings["latitude"]}')
+logger.info(f'Longitude: {settings["longitude"]}')
+logger.info(f'Elevation: {settings["elevation_m"]}')
+
+
+station = Topos(
+    latitude=settings['latitude'],
+    longitude=settings['longitude'],
+    elevation_m=settings['elevation_m']
+)
+
+load = Loader(
+    'skyfield/',
+    verbose=True,
+    expire=True
+)
+ts = load.timescale()
+
+satellites = load.tle('http://celestrak.com/NORAD/elements/weather.txt')
+logger.info('TLEs loaded.')
+
+satellite = satellites['NOAA 19']
+logger.info(satellite)
+
+t, events = satellite.find_events(
+    station,
+    ts.utc(datetime.now(timezone.utc)),
+    ts.utc(datetime.now(timezone.utc) + timedelta(days=1)),
+    altitude_degrees=15
+)
+
+logger.info(t)
+logger.info(events)
